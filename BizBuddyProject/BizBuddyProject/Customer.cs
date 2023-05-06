d0ac5b5a2bd3ed239dbc7f103b4a417a21325c7e
Niña Jane Tubigon <21-1-00632@vsu.edu.ph>
2023-05-06 19:51:38 +0800

    retrieve old save

--- BizBuddyProject/BizBuddyProject/Customer.cs
+++ BizBuddyProject/BizBuddyProject/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,34 @@ namespace BizBuddyProject.Classes
 {
     public class Customer
     {
+        private int id;
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private Dictionary<string, List<string>> contacts = new Dictionary<string, List<string>>();
+        
+        public int ID
+        {
+            get => this.id; 
+            set => this.id = value;
+        }
 
+        public string FirstName
+        {
+            get => this.firstName; 
+            set => this.firstName = value;
+        }
+
+        public string MiddleName
+        {
+            get => this.middleName;
+            set => this.middleName = value;
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+            set => this.lastName = value;
+        }
     }
 }
