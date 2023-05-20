c18cba09b2d4800801d1f064d68dbb3322bfcd66
janetub <janetubigon00@gmail.com>
2023-05-20 16:46:24 +0800

    restructured class

--- lib/classes/Item.dart
+++ lib/classes/Item.dart
@@ -1,10 +1,10 @@
 class Item
 {
-  String name;
-  double cost;
-  int quantity;
-  DateTime dateBought;
-  Set<String> tags;
+  String name = '';
+  double cost = 0;
+  int quantity = 0;
+  DateTime dateBought = DateTime.now();
+  Set<String>? tags;
 
-  Item({this.name, this.cost, this.quantity, this.dateBought, this.tags});
+  Item(this.name, this.cost, this.quantity, this.dateBought, this.tags);
 }
--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -26,3 +26,17 @@ class MainCanvas extends StatelessWidget
     );
   }
 }
+
+class AddButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const AddButton({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: const Icon(Icons.add),
+    );
+  }
+}
