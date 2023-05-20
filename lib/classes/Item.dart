ee61d6245af3c38e278b9f49eea51e5f693088ad
janetub <janetubigon00@gmail.com>
2023-05-20 18:02:15 +0800

    restructure class

--- lib/classes/Item.dart
+++ lib/classes/Item.dart
@@ -1,10 +1,29 @@
-class Item
-{
-  String name = '';
-  double cost = 0;
-  int quantity = 0;
-  DateTime dateBought = DateTime.now();
+class Item {
+  String name;
+  double _cost = 0;
+  int _quantity = 1;
+  DateTime? dateBought = DateTime.now();
   Set<String>? tags;
 
-  Item(this.name, this.cost, this.quantity, this.dateBought, this.tags);
+  Item(this.name, this.tags);
+
+  double get cost => _cost;
+
+  set cost(double value) {
+    if (value >= 0) {
+      _cost = value;
+    } else {
+      throw ArgumentError('Cost cannot be negative.');
+    }
+  }
+
+  int get quantity => _quantity;
+
+  set quantity(int value) {
+    if (value >= 1) {
+      _quantity = value;
+    } else {
+      throw ArgumentError('Quantity must be at least 1.');
+    }
+  }
 }
